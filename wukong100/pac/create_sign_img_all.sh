dedfdd6beb12a77456f5c021fcc17a0d1a273a5a
yangyang <yangyang@superred.com.cn>
2026-03-27 10:09:31 +0800

    Description: 修改license  是否完成编程规范自检:Y 是否编程且验证通过:Y 影响的设备与平台范围:wukong100 团队:中科鸿略
    Signed-off-by: yangyang <yangyang@superred.com.cn>

--- wukong100/pac/create_sign_img_all.sh
+++ wukong100/pac/create_sign_img_all.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
-# Copyright (C) 2026 Beijing Zhongke Honglve Technology Co., Ltd. All rights reserved.
+# Copyright (C) 2026 Beijing Zhongke Honglve Technology Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# SPDX-License-Identifier: GPL-3.0+
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-# This software is proprietary to Zhongke Honglve Technology Co., Ltd. and may only be used
-# in accordance with the terms of the GNU General Public License version 3 or any later version.
-# Unauthorized copying, modification, or distribution of this software is strictly prohibited.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -e
 set -v
